3545d43cf852b832302afbb55c9a56508b42f8ad
carloseocandido <carloseocandido@gmail.com>
2022-10-29 17:52:49 -0300

    create cards de produtos

---
+++ cabacalho.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $titulo; ?></title>
+  <link rel="stylesheet" href="./assets/bootstrap.min.css" />
+  <link rel="stylesheet" href="./assets/test.css" />
+  <style>
+    /* div {
+            border: 2px solid red;
+        } */
+  </style>
+</head>
+
+<body>
+  <nav id=tes class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+
+        <img src="./assets/Ragute.png" alt="Bootstrap" width="220" height="50">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a id="test" class="nav-link active" aria-current="page" href="./index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a id="test" class="nav-link active" aria-current="page" href="./lanches.php">Lanches</a>
+          </li>
+          <li class="nav-item">
+            <a id="test" class="nav-link active" aria-current="page" href="#">Porções</a>
+          </li>
+          <li class="nav-item">
+            <a id="test" class="nav-link active" aria-current="page" href="#">Bebidas</a>
+          </li>
+          <li class="nav-item">
+            <a id="test" class="nav-link active" aria-current="page" href="#">Sobre</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+  <div class="container">
---
+++ lanches.php
@@ -0,0 +1,128 @@
+<?php include "./cabacalho.php"; ?>
+<div class="container-xl py-4 py-xl-5">
+
+
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Lanches</li>
+    </ol>
+    </nav>
+
+    <h2 class="text-center">Lanches</h2>
+    <h3 class="p-3">Hamburguer</h2>
+
+
+    <div class="row">
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                <img src="./assets/lanche.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-5">
+                <div class="card-body">
+                    <h5 class="card-title">X-Bacon</h5>
+                    <p class="card-text">IngredientesIngredientesIngredientesIngredientesIngredientesIngredientesIngredientes</p>
+                    <h6 class="card-title">R$15,00</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+</div>
+
+        
+
+       
+        
+
+<?php include "./rodape.php"; ?>
