caeb6f1c7734112f53f35b41f77b9427220712ac
carloseocandido <carloseocandido@gmail.com>
2022-11-01 15:13:59 -0300

    corrige bugs

---
+++ rodape.php
@@ -0,0 +1,30 @@
+</div>
+
+</body>
+        <footer class="content-fluid">
+            <div class="row">
+                <div class="col-md-1 col-sm-12"></div>
+                <div class="col-md-3 col-sm-12 text-center">
+                    <img src="./assets/RaguteFooter.png" alt="Bootstrap" width="152px" height="152px" >
+                    <script src="./assets/bootstrap.bundle.min.js"></script>
+                </div>
+                <div class="col-md-4 col-sm-12 text-center ">
+                    <p>Contato</p>
+                    <p>Telefone: (00) 4002-8922</p>
+                    <p>Whatsapp: (00) 00000-0000</p>
+                    <p>Email: email.@email.com.br</p>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+
+                    <p class="tx_sl text-end">&copy; Copyright <?php echo date('Y') ?>  Ragute Team</p>
+
+                </div>
+                <div class="col-md-1 col-sm-12"></div>
+            </div>
+        </footer>
+</html>
