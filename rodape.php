670472ede2f2aad2412b5b978df480e1a3c0d6c3
NotUser5 <jmtorrichelli@gmail.com>
2022-11-03 15:47:52 -0300

    Rodape updated

--- rodape.php
+++ rodape.php
@@ -6,19 +6,15 @@
             <div class="row">
                 <div class="col-md-1 col-sm-12"></div>
                 <div class="col-md-3 col-sm-12 text-center">
-                    <img src="./assets/RaguteFooter.png" alt="Bootstrap" width="152px" height="152px" >
+                    <img src="./assets/RaguteFooter.png" alt="Bootstrap" width="160px" height="160px" >
                     <script src="./assets/bootstrap.bundle.min.js"></script>
                 </div>
                 <div class="col-md-4 col-sm-12 text-center ">
                     <p>Contato</p>
-                    <p>Telefone: (00) 4002-8922</p>
-                    <p>Whatsapp: (00) 00000-0000</p>
-                    <p>Email: email.@email.com.br</p>
-                </div>
-                <div class="col-md-3 col-sm-12">
-
-                    <p class="tx_sl text-end align-text-bottom" id="copy">&copy; Copyright <?php echo date('Y') ?>  Ragute Team</p>
-
+                    <p id="cont">Telefone: (00) 4002-8922</p>
+                    <p id="cont">Whatsapp: (00) 00000-0000</p>
+                    <p id="cont">Email: email.@email.com.br</p>
+                    <p class="tx_sl" id="copy">&copy; Copyright <?php echo date('Y') ?>  Ragute Team</p>
                 </div>
                 <div class="col-md-1 col-sm-12"></div>
             </div>
