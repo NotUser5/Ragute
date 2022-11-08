b75f4d137195b1ea19e8e0b5afae58bd70cad737
NotUser5 <jmtorrichelli@gmail.com>
2022-11-08 16:39:31 -0300

    Add Sobre

---
+++ sobre.php
@@ -0,0 +1,117 @@
+<?php include "./cabecalho.php"; ?>
+<div class="container-xl py-4 py-xl-5">
+
+
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Sobre</li>
+        </ol>
+    </nav>
+
+
+    <center>
+        <div class="container">
+            <h1><strong>A EMPRESA</strong></h1>
+            <hr>
+    </center>
+    <div class="container">
+        <p style="font-family: 'Varela Round', sans-serif;" align="justify">
+            A <strong>Ragute</strong> foi fundada em 2022 na cidade de Ribeirão Preto, localizada no estado de São Paulo. Tem atuação no ramo alimentício e é composta pelos Co Fundadores Carlos Eduardo Torrichelli de Souza Pinto, Carlos Folgado Emanuel de Oliveira Cândido, Isabela Furriel Palone, João Marcos Torrichelli Mattos, Johnny Rodrigues Evangelista e Lucas Sgarbi Aravéchia.<br><br>
+            <strong>Missão</strong>: Fornecer agilidade para nossos clientes ao escolher suas refeições. <br>
+            <strong>Visão</strong>: Ajudar mais empresas a fornecer redução de tempo e entendimento sobre seus produtos de forma clara aos clientes, ao realizarem seus pedidos no estabelecimento ou delivery. <br>
+            <strong>Valores</strong>: Agilidade e Qualidade.
+        </p>
+    </div>
+
+
+
+    <div class="row">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="thumbnail">
+                    <center><img src="./assets/logo_ragute.png" class="img-responsive" style="width:500px; height:500px;" alt="imagem"></center>
+                </div>
+            </div>
+
+
+
+        </div>
+
+        <div class="container" style="margin-top:25px;">
+            <div class="row">
+                <div class="footer-col col-md-12">
+                    <center>
+                        <adress>
+
+                            <h3>Endereço</h3>
+                            <p style="font-size:18px">Ragute<br>
+                                Rua São José, 1320<br>
+                                Ribeirão Preto - SP</p>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.1867418709185!2d-47.81315238444801!3d-21.184739384018624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b9bee5ce011959%3A0xc5b8ab40850a6edb!2sR.%20S%C3%A3o%20Jos%C3%A9%2C%201320%20-%20Centro%2C%20Ribeir%C3%A3o%20Preto%20-%20SP%2C%2014010-160!5e0!3m2!1spt-BR!2sbr!4v1667328578728!5m2!1spt-BR!2sbr" width="100%" height="300px" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+                            <center>
+                                <h3>Contato</h3>
+                                <p style="font-size:18px">Telefone</p>
+                            </center>
+                            <p style="font-size:18px"><a href="tel:01632025980">(16)3202-5980</a></p>
+
+                            <center>
+                                <p style="font-size:18px">E-mail</p>
+                            </center>
+                            <a class="btn btn-success btn-sm" href="mailto:raguteburger@gmail.comSubject=O%20que%20deseja?" target="_top">Nos envie um e-mail clicando aqui!</a>
+                        </adress>
+                    </center>
+                </div>
+
+
+
+                <div class="footer-col col-md-12">
+                    <center>
+                        <h3> Nos encontre nas redes sociais</h3>
+
+                        <ul class="list-inline">
+
+                            <li>
+                                <a href="https://www.facebook.com/profile.php?id=100087576552106" target="_blank" class="btn btn-primary">
+                                    <span class="sr-only">FACEBOOK</span>
+                                    <i class="fa fa-fw fa-facebook fa-2x"></i>
+                                </a>
+                                <a href="https://twitter.com/ragute_burger" target="_blank" class="btn btn-primary">
+                                    <span class="sr-only">TWITTER</span>
+                                    <i class="fa fa-fw fa-twitter fa-2x"></i>
+                                </a>
+                                <a href="https://twitter.com/login?lang=pt" target="_blank" class="btn btn-primary">
+                                    <span class="sr-only">INSTAGRAM</span>
+                                    <i class="fa fa-fw fa-instagram fa-2x"></i>
+                                </a>
+
+
+                            </li>
+                            <br>
+                        </ul>
+
+
+
+                    </center>
+
+                </div>
+
+                <br>
+
+                <center><a href="index.php" class="btn btn-danger btn-lg" role="button">Voltar</a></center>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</div>
+
+
+
+
+<?php include "./rodape.php"; ?>
