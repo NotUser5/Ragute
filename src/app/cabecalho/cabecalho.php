888c71c1154a163dcf4e4d4fc7ab5af383ff8dc3
Jao <jmtorrichelli@gmail.com>
2022-11-09 20:58:09 -0300

    ico

--- src/app/cabecalho/cabecalho.php
+++ src/app/cabecalho/cabecalho.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo; ?></title>
+    <link rel="icon" type="image/ico" href="../../assets/img/logo-ico.ico"/>
     <link rel="stylesheet" href="../../style/bootstrap.min.css" />
     <link rel="stylesheet" href="../../style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
