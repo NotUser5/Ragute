c835b5202cbe785be9f81f67ba40890cf556c1f7
carloseocandido <carloseocandido@gmail.com>
2022-11-09 18:02:31 -0300

    img add

--- form/formCadProdutos.php
+++ form/formCadProdutos.php
@@ -1,26 +1,49 @@
-<?php include "./uCabecalho.php"; if(isset($_POST) && !empty($_POST)){
-
-
+<?php include "./uCabecalho.php"; 
+$titulo = "Cadastro de Usuario";
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+if(isset($_POST) && !empty($_POST)){
+echo '<div class="alert alert-success">
+Cadastrado com sucesso
+</div>';
 include "./conexao.php";
 $descricao = $_POST["descricao"];
 $valor = $_POST["valor"];
 $ingredientes = $_POST["ingredientes"];
-$select = $_POST["select"];
-$imagem = $_POST["imagem"];
+// $select = $_POST["select"];
+if(isset($FILES["imagem"]) && !empty($_FILES["imagem"])){
+    $imagem = "./img/".$_FILES["imagem"]["name"];
+    move_uploaded_file($_FILES["imagem"]["tmp_name"], $imagem);
+    echo "update realizado com sucesso";
+} else {
+    $imagem = "NULL";
+}
+
+// $imagem = $_POST["imagem"];
+
 if(isset($_POST["ativo"]) && $_POST["ativo"] == "on"){
     $ativo = 1;
 } else {
     $ativo = 0;
 }
 
-$query = "insert into usuarios (DESCRICAO, VALOR, IMG, ATIVO, INGREDIENTES) values ('$descricao', '$valor', '$ingredientes', '$select', '$imagem', $ativo)";
+$query = "insert into produtos (ID, DESCRICAO, VALOR, INGREDIENTES, IMG) VALUES ('NULL', '$descricao', '$valor', '$ingredientes', '$imagem', $ativo,)";
 $resultado = mysqli_query($conexao, $query);
 if($resultado){
-    header("Location: ./login.php");
+    header("Location: ./formCadProdutos.php");
     exit();
     ?>
         <div class="alert alert-success">
             Cadastrado com sucesso
+        </div>
+    <?php
+} else {
+    header("Location: ./formCadProdutos.php");
+    exit();
+    ?>
+        <div class="alert alert-danger">
+            Erro!
         </div>
     <?php
 }
@@ -28,8 +51,6 @@ if($resultado){
 }
 
 ?>
-
-
 
 <div class="row">
     <div class="offset-4 col-md-4">
@@ -39,21 +60,21 @@ if($resultado){
                 <form action="./formCadProdutos.php" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label>Descrição</label>
-                        <input type="text" name="descricao" class="form-control" style="width: 100%;" />
+                        <input type="text" name="descricao" class="form-control" />
                     </div>
 
                     <div class="form-group" id="inserir">
                         <label>Valor</label>
-                        <input type="number" name="valor" class="form-control" style="width: 100%;" />
+                        <input type="number" name="valor" class="form-control" />
                     </div>
 
                     <div class="form-group" id="inserir">
                         <label>Ingredientes</label>
-                        <input type="text" name="ingredientes" class="form-control" style="width: 100%;" />
+                        <input type="text" name="ingredientes" class="form-control" />
                     </div>
-
+<!-- 
                     <div class="form-group text-center" id="inserir">
-                        <select class="form-select" aria-label="Default select example" name="select">
+                        <select class="form-select" name="select">
                             <option selected>Selecione a Categoria</option>
                             <option value="1">Hamburger</option>
                             <option value="2">Frango</option>
@@ -62,16 +83,13 @@ if($resultado){
 
                         <div class="form-group" id="inserir">
                             <label>Selecione a Imagem</label>
-                            <input type="file" name="imagem" id="upImg" accept="image/*" class="form-control" style="width: 100%;" />
-                            <button type="submit" class="btn btn-success" id="btnUpImg">
-                                Enviar Imagem
-                            </button>
+                            <input type="file" name="imagem" id="upImg" accept="image/*" class="form-control" />
                         </div>
 
                         <div class="form-check text-center" id="inserir">
                             Ativo <input type="checkbox" name="ativo">
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="form-group text-center" id="inserir" id="btnSalvar">
                         <button type="submit" class="btn btn-success">
