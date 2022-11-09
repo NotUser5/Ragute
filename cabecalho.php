b8dd89014bfc9fd6c7461b0047abe3681c0db669
NotUser5 <jmtorrichelli@gmail.com>
2022-11-09 16:37:09 -0300

    ico

--- cabecalho.php
+++ cabecalho.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo; ?></title>
+    <link rel="icon" type="image/ico" href="./assets/logo-ico.ico"/>
     <link rel="stylesheet" href="./assets/bootstrap.min.css" />
     <link rel="stylesheet" href="./assets/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
