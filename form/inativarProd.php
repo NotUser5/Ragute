4a0bb18e817d7928c15873dc4c9caf070cca20e6
TrueCebola <carloseduardotdes@gmail.com>
2022-11-23 17:10:38 -0300

    criado php pra inativar produto ativo

---
+++ form/inativarProd.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET["id"]) && !empty($_GET["id"])){
+    include "conexao.php";
+    $query = "update produtos set ativo= 0 where id = ".$_GET["id"];
+    $resultado = mysqli_query($conexao,$query);
+    if($resultado){
+        header("location: tabProdutos.php?sucesso=Inativado com sucesso");
+        exit();
+    }
+    else{
+        header("location: tabProdutos.php?erro=Ocorreu algum erro no banco");
+        exit();
+    }
+}
+else{
+    header("location: tabProdutos.php?erro=Selecione o produto para inativar");
+    exit();
+}
+?>
