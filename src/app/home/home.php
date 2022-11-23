564725fc03fb1162a1f44461a25a8db32cfa9d21
LucasSgarbi <ls.aravechia@hotmail.com>
2022-11-23 20:44:34 -0300

    att home

--- src/app/home/home.php
+++ src/app/home/home.php
@@ -1,6 +1,10 @@
 <?php $titulo = "Home"; include "../cabecalho/cabecalho.php"; ?>
 
 <div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 col-sm-12" > <h4>Bem Vindo ao cardápio do Ragute</h4>  </div>
+    </div>
+<h1 id="categoria" > Promoções</h1>
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
